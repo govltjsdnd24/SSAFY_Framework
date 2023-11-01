81c5cbb36d70166ee83755d6532b99ccb1948b61
Sun Woong Chang <38126462+govltjsdnd24@users.noreply.github.com>
2023-11-01 09:09:29 +0900

    1101_1

--- MemberSpringBootMVC/src/main/java/edu/ssafy/mvc/HomeController.java
+++ MemberSpringBootMVC/src/main/java/edu/ssafy/mvc/HomeController.java
@@ -17,9 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import io.swagger.annotations.Api;
-
-
 @Controller
 public class HomeController {
 	
